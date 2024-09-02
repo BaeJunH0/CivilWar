8bef3b9f1d97bf26782d6d7b11ff02a87b982189
BaeJunho <bjh4498@knu.ac.kr>
2024-09-02 19:52:23 +0900

    refactor : domain 연관관계 변경
    user와 다대일, team과 player 사이는 다대다로 결정

--- src/main/java/toyProject/demo/domain/Player.java
+++ src/main/java/toyProject/demo/domain/Player.java
@@ -2,28 +2,41 @@ package toyProject.demo.domain;
 
 import jakarta.persistence.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Table(name="Player")
 @Entity
 public class Player {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name="level")
+    @Column(name="level", nullable = false)
     private int level;
-    @Column(name="nickname")
+    @Column(name="nickname", nullable = false)
     private String nickname;
     @Column(name="freeTier")
     private String freeTier;
     @Column(name="soloTier")
     private String soloTier;
+    @ManyToOne
+    private User user;
+    @ManyToMany
+    @JoinTable(
+            name = "player_team",
+            joinColumns = @JoinColumn(name = "player_id"),
+            inverseJoinColumns = @JoinColumn(name = "team_id")
+    )
+    private Set<Team> teams = new HashSet<>();
 
     protected Player(){}
 
-    public Player(int level, String nickname, String freeTier, String soloTier) {
+    public Player(int level, String nickname, String freeTier, String soloTier, User user) {
         this.level = level;
         this.nickname = nickname;
         this.freeTier = freeTier;
         this.soloTier = soloTier;
+        this.user = user;
     }
 
     public int getLevel() {
@@ -40,5 +53,13 @@ public class Player {
 
     public String getSoloTier() {
         return soloTier;
+    }
+
+    public User getUser(){
+        return user;
+    }
+
+    public void joinTeam(Team team){
+        teams.add(team);
     }
 }
--- src/main/java/toyProject/demo/domain/Team.java
+++ src/main/java/toyProject/demo/domain/Team.java
@@ -2,7 +2,8 @@ package toyProject.demo.domain;
 
 import jakarta.persistence.*;
 
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 @Table(name="Team")
 @Entity
@@ -10,22 +11,29 @@ public class Team {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name = "name")
+    @Column(name = "name", unique = true)
     private String name;
-    @OneToMany
-    private List<Player> players;
+    @ManyToOne
+    private User user;
+    @ManyToMany(mappedBy = "teams")
+    private Set<Player> players = new HashSet<>();
 
     protected Team(){}
 
-    public Team(String name) {
+    public Team(String name, User user) {
         this.name = name;
+        this.user = user;
     }
 
     public String getName() {
         return name;
     }
 
-    public List<Player> getPlayers() {
-        return players;
+    public User getUser() {
+        return user;
+    }
+
+    public void addPlayer(Player player){
+        players.add(player);
     }
 }
