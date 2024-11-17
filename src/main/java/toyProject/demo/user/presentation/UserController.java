e0c28f1401a91d1e87182ed1a14b7cacc71a657a
BaeJunho <bjh4498@knu.ac.kr>
2024-11-17 19:04:52 +0900

    refactor : lombok 활용하여 생성자 주입 구현

--- src/main/java/toyProject/demo/user/presentation/UserController.java
+++ src/main/java/toyProject/demo/user/presentation/UserController.java
@@ -1,5 +1,6 @@
 package toyProject.demo.user.presentation;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,13 +11,10 @@ import toyProject.demo.user.application.UserService;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/users")
 public class UserController {
     private final UserService userService;
-
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
 
     @GetMapping
     public ResponseEntity<List<UserResponse>> readUsers(){
