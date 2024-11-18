1fe7315b2e697ac4675100c08360bc958e4d872f
BaeJunho <bjh4498@knu.ac.kr>
2024-11-18 17:05:47 +0900

    refactor : application dto 분리

--- src/main/java/toyProject/demo/user/application/dto/UserCommand.java
+++ src/main/java/toyProject/demo/user/application/dto/UserCommand.java
@@ -1,4 +1,25 @@
 package toyProject.demo.user.application.dto;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import toyProject.demo.user.domain.User;
+import toyProject.demo.user.presentation.dto.UserRequest;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
 public class UserCommand {
+    private String email;
+    private String nickname;
+    private String password;
+
+    public static UserCommand from(UserRequest userRequest){
+        return new UserCommand(
+                userRequest.getEmail(), userRequest.getNickname(), userRequest.getPassword()
+        );
+    }
+
+    public static User toEntityFrom(UserCommand userCommand){
+        return User.of(userCommand.getEmail(), userCommand.getPassword(), userCommand.getNickname());
+    }
 }
