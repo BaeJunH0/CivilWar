c25c5322dad7287d529b7db356dae1cf0a1dc895
BaeJunho <bjh4498@knu.ac.kr>
2025-02-17 16:20:05 +0900

    feat : 어드민 페이지 연결을 위한 컨트롤러 설정

--- src/main/java/toyProject/demo/admin/presentation/AdminPageController.java
+++ src/main/java/toyProject/demo/admin/presentation/AdminPageController.java
@@ -1,7 +1,12 @@
 package toyProject.demo.admin.presentation;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AdminPageController {
+    @GetMapping("/adminLogin")
+    public String adminLogin() {
+        return "adminLogin.html";
+    }
 }
