309955c8dd4a50863d9d83b811f2a6bf8ea718bb
BaeJunho <bjh4498@knu.ac.kr>
2025-02-17 15:40:19 +0900

    refactor : 위치 변경, 사용안하는 파일 삭제

--- src/main/java/toyProject/demo/player/persistence/PlayerRepository.java
+++
@@ -1,7 +0,0 @@
-package toyProject.demo.player.persistence;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import toyProject.demo.player.domain.Player;
-
-public interface PlayerRepository extends JpaRepository<Player, Long> {
-}
--- src/main/java/toyProject/demo/team/application/dto/TeamCommand.java
+++
@@ -1,9 +0,0 @@
-package toyProject.demo.team.application.dto;
-
-import toyProject.demo.team.domain.Team;
-
-public record TeamCommand(String name) {
-    public static TeamCommand from(Team team){
-        return new TeamCommand(team.getName());
-    }
-}
